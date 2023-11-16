bcef6acc4b8db32d44ff0dbdab3ed308d903a726
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-16 16:13:38 +0100

    Aggiungere i file di progetto.

---
+++ MinAndMax/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new Random();
+
+List<int> numeriCasuali = new List<int>();
+
+for(int i = 0; i < 1000; i++)
+{
+    int numeroCasuale = random.Next();
+
+    numeriCasuali.Add(numeroCasuale);
+}
+
+foreach (int numeroCasuale in numeriCasuali)
+{
+    Console.WriteLine(numeroCasuale + "\n");
+ 
+}
+
+
+int numeroMinimo = numeriCasuali.Min();
+int numeroMassimo = numeriCasuali.Max();
+
+Console.WriteLine($"Numero Minimo/Casuale {numeroMinimo}:\n");
+Console.WriteLine($"Numero Massimo/Casuale {numeroMassimo}:\n");
